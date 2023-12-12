c1027720ac0d9854bb815f5c53fd1c5cd1f3bd49
Tim <Azacar02@gmail.com>
2023-12-12 13:39:02 -0500

    Wrote code for last of routes: /theaters.

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -4,6 +4,15 @@ function list() {
   return knex("theaters").select("*");
 }
 
+function getMovies(theaterId) {
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "mt.theater_id", "t.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .select("m.*", "mt.is_showing")
+    .where({ "t.theater_id": theaterId });
+}
+
 module.exports = {
   list,
+  getMovies,
 };
